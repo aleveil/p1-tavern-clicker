182fe701b5aa93d9f96bde24d73723b7457f5c35
aleveil <anthony.leveil@protonmail.com>
2023-03-16 10:45:40 +0100

    put create/update functions on Upgrade class and add dynamic display with upgrades arrays

--- class/Upgrade.js
+++ class/Upgrade.js
@@ -15,6 +15,8 @@ class Upgrade {
 
 		this.isAuto = _isAuto;
 
+		this.elementData = null;
+
 		if (this.isAuto)
 			setInterval(() => { this.earn() }, 1000);
 	}
@@ -25,6 +27,7 @@ class Upgrade {
 		{
 			addBeer(currentIncome);
 			this.totalIncome += currentIncome;
+			this.updateElement();
 		}
 	}
 
@@ -37,9 +40,65 @@ class Upgrade {
 			addBeer( -(this.price) );
 			this.price = Math.ceil(this.price * this.priceMultiplicator);
 			this.quantity++;
+			this.updateElement();
 			return true;
 		}
 		else
 			return false;
+	}
+
+	createElement(parentElement) {
+		// create elements
+		const container = document.createElement("div");
+		const title = document.createElement("h3");
+		const quantity = document.createElement("p");
+		const income = document.createElement("p");
+		const incomePerUnit = document.createElement("p");
+		const totalIncome = document.createElement("p");
+		const buyButton = document.createElement("button");
+	
+		// assign class/id
+		container.classList.add("upgrade-container");
+		container.id = `upgrade-${this.id}`;
+		title.classList.add("upgrade-title");
+		quantity.classList.add("upgrade-quantity");
+		income.classList.add("upgrade-income");
+		incomePerUnit.classList.add("upgrade-incomePerUnit");
+		totalIncome.classList.add("upgrade-totalIncome");
+		buyButton.classList.add("upgrade-buyButton");
+	
+		// append elements
+		container.appendChild(title);
+		container.appendChild(quantity);
+		container.appendChild(income);
+		container.appendChild(incomePerUnit);
+		container.appendChild(totalIncome);
+		container.appendChild(buyButton);
+		
+		// buy button event
+		buyButton.addEventListener("click", () => {this.tryToBuy()});
+
+		// create and assign elementData object
+		this.elementData = {
+			container: container,
+			title: title,
+			quantity: quantity,
+			income: income,
+			incomePerUnit: incomePerUnit,
+			totalIncome: totalIncome,
+			buyButton: buyButton
+		};
+
+		parentElement.appendChild(container);
+		this.updateElement();
+	}
+
+	updateElement() {
+		this.elementData.title.innerText = this.title;
+		this.elementData.quantity.innerText = `Quantity : ${this.quantity}🍺`;
+		this.elementData.income.innerText = `Income : ${this.quantity * this.incomePerUnit}🍺`;
+		this.elementData.incomePerUnit.innerText = `Income/Unit : ${this.incomePerUnit}🍺`;
+		this.elementData.totalIncome.innerText = `Total Income : ${this.totalIncome}🍺`;
+		this.elementData.buyButton.innerHTML = `BUY<br />[${this.price}🍺]`;
 	}
 }
--- script.js
+++ script.js
@@ -2,6 +2,25 @@
 const mainButton = document.querySelector(".main-button");
 const beerCounterElement = document.querySelector(".counter");
 
+const manualUpgradesContainer = document.querySelector("#manual-column");
+const autoUpgradesContainer = document.querySelector("#auto-column");
+
+// manage upgrades
+
+const upgrades = {
+	manuals: [],
+	automatics: []
+}
+
+upgrades.manuals.push(new Upgrade(1, "Serveur", "Un habile serveur.", 1, 10, false, 1));
+upgrades.automatics.push(new Upgrade(2, "Habitué", "Un client ivre.", 2, 50, true));
+upgrades.automatics.push(new Upgrade(3, "Barman", "Un barman accueillant.", 5, 250, true));
+
+// create upgrades elements
+upgrades.manuals.forEach(upgrade => {upgrade.createElement(manualUpgradesContainer)});
+upgrades.automatics.forEach(upgrade => {upgrade.createElement(autoUpgradesContainer)});
+
+// manage beers
 let beers = 0;
 
 function updateBeersDisplay() {
@@ -13,69 +32,10 @@ function addBeer(amount) {
 	updateBeersDisplay();
 }
 
-const upgradeManual = new Upgrade(1, "Serveur", "Un serveur efficace.", 1, 10, false, 1);
-
-mainButton.addEventListener("click", () => {
-	upgradeManual.earn();
-});
-
-// update display on start
+// update beers display on start
 updateBeersDisplay();
 
-
-const manualUpgradesContainer = document.querySelector("#manual-column");
-const autoUpgradesContainer = document.querySelector("#auto-column");
-
-function createUpgradeElement(index) {
-	// create elements
-	const container = document.createElement("div");
-	const title = document.createElement("h3");
-	const quantity = document.createElement("p");
-	const income = document.createElement("p");
-	const incomePerUnit = document.createElement("p");
-	const totalIncome = document.createElement("p");
-	const buyButton = document.createElement("button");
-
-	// assign class/id
-	container.classList.add("upgrade-container");
-	container.id = `upgrade-${index}`;
-	title.classList.add("upgrade-title");
-	quantity.classList.add("upgrade-quantity");
-	income.classList.add("upgrade-income");
-	incomePerUnit.classList.add("upgrade-incomePerUnit");
-	totalIncome.classList.add("upgrade-totalIncome");
-	buyButton.classList.add("upgrade-buyButton");
-
-	// append elements
-	container.appendChild(title);
-	container.appendChild(quantity);
-	container.appendChild(income);
-	container.appendChild(incomePerUnit);
-	container.appendChild(totalIncome);
-	container.appendChild(buyButton);
-
-	return (container);
-}
-
-function updateUpgradeElement(upgrade) {
-	const container = document.querySelector(`#upgrade-${upgrade.id}`);
-	const title = document.querySelector(`#upgrade-${upgrade.id} .upgrade-title`);
-	const quantity = document.querySelector(`#upgrade-${upgrade.id} .upgrade-quantity`);
-	const income = document.querySelector(`#upgrade-${upgrade.id} .upgrade-income`);
-	const incomePerUnit = document.querySelector(`#upgrade-${upgrade.id} .upgrade-incomePerUnit`);
-	const totalIncome = document.querySelector(`#upgrade-${upgrade.id} .upgrade-totalIncome`);
-	const buyButton = document.querySelector(`#upgrade-${upgrade.id} .upgrade-buyButton`);
-
-	title.innerText = upgrade.title;
-	quantity.innerText = `Quantity : ${upgrade.quantity}`;
-	income.innerText = `Income : on sait po`;
-	incomePerUnit.innerText = `Income/Unit : ${upgrade.incomePerUnit}`;
-	totalIncome.innerText = `Total Income : ${upgrade.totalIncome}`;
-	buyButton.innerText = `Buy ${upgrade.price}`;
-}
-
-
-manualUpgradesContainer.appendChild( createUpgradeElement(1) );
-updateUpgradeElement(upgradeManual);
-
-
+// main click button event
+mainButton.addEventListener("click", () => {
+	upgrades.manuals.forEach(upgrade => {upgrade.earn()});
+});
